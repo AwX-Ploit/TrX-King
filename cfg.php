1d4fa8f9f4eb752b78ccb383ca56f9cdd22c6e9b
It's Me <77188967+AwX-Ploit@users.noreply.github.com>
2021-10-04 19:25:45 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+// Author      : AwX-Ploit
+// Description : JANGAN MERUBAH NAMA VARIABEL DATA!
+
+
+// TINGGAL PASTE DI TANDA KUTIP
+
+$useragent ="   ";
+$accept_language ="   ";
+$csrf_cookie_name ="   ";
+$ci_session ="   ";
